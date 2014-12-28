16dd9c025c49755693ecc70da741350208808ee0
Francois Ritaly <francois.ritaly@gmail.com>
2014-12-28 20:30:42 +0100

    Added solution to problem 1 ("Multiples of 3 and 5")

---
+++ src/main/java/com/github/fritaly/projecteuler/Problem1.java
@@ -0,0 +1,21 @@
+package com.github.fritaly.projecteuler;
+
+/**
+ * Problem "Multiples of 3 and 5".
+ *
+ * @author francois_ritaly
+ */
+public class Problem1 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int i = 1; i < 10000; i++) {
+			if ((i % 3 == 0) || (i % 5 == 0)) {
+				sum += i;
+			}
+		}
+
+		System.out.println("Sum: " + sum);
+	}
+}
