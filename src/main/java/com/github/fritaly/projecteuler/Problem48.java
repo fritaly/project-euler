b3c8fc9b3d3668dc352a19e8f0caf41a0bdf970a
Francois Ritaly <francois.ritaly@gmail.com>
2015-01-04 12:14:46 +0100

    Added solution to problem 48 ("Self powers")

---
+++ src/main/java/com/github/fritaly/projecteuler/Problem48.java
@@ -0,0 +1,24 @@
+	package com.github.fritaly.projecteuler;
+
+import java.math.BigInteger;
+
+
+/**
+ * Problem "Self powers".
+ *
+ * @author francois_ritaly
+ */
+public class Problem48 {
+
+	public static void main(String[] args) {
+		BigInteger result = BigInteger.ZERO;
+
+		for (int i = 1; i <= 1000; i++) {
+			result = result.add(BigInteger.valueOf(i).pow(i));
+		}
+
+		final String text = result.toString();
+
+		System.out.println(text.substring(text.length() - 10));
+	}
+}
