c1ee6bb811b97fdc6c3410fdaced5bc5f2b07c1d
Francois Ritaly <francois.ritaly@gmail.com>
2015-01-05 21:30:32 +0100

    Optimised solution for Problem9 ("Special Pythagorean triplet")

--- src/main/java/com/github/fritaly/projecteuler/Problem9.java
+++ src/main/java/com/github/fritaly/projecteuler/Problem9.java
@@ -13,13 +13,13 @@ public class Problem9 {
 
 	public static void main(String[] args) {
 		// "a < b < c" && "a + b + c = 1000"
-		for (int c = 1; c < 1000; c++) {
-			for (int b = 1; (b < c) && (b + c < 1000); b++) {
-				for (int a = 1; (a < b) && (a + b + c <= 1000); a++) {
-					if ((a + b + c == 1000) && isSpecialTriplet(a, b, c)) {
-						System.out.println("Result: " + (a * b * c));
-						break;
-					}
+		for (int a = 1; a < 1000; a++) {
+			for (int b = a + 1; (a + b) < 1000; b++) {
+				final int c = 1000 - a - b;
+
+				if (isSpecialTriplet(a, b, c)) {
+					System.out.println("Result: " + (a * b * c));
+					break;
 				}
 			}
 		}
